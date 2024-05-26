af07abff65159cd3d80b2f76408d2f9d7b632f9d
hanna-8l <152915440+hanna-8l@users.noreply.github.com>
2024-05-26 17:12:28 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import SearchEngine from './SearchEngine';
+import SearchEngine from 'src/SearchEngine.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ForestHub React Search Engine</h1>
+        <SearchEngine />
       </header>
     </div>
   );
