58fe6df46abd3948782998a79dc048ef9531c8d4
hanna-8l <152915440+hanna-8l@users.noreply.github.com>
2024-12-01 20:50:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css';
+// import './App.css';
 import SearchBar from './SearchBar';
 import WeatherService from './weatherService';
 
